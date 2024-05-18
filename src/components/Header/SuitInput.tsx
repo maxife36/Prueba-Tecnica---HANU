31fd5f236c931aaf80460f6b1183382cbe3b8845
maxife36 <maxife36@gmail.com>
2024-05-18 05:08:52 -0300

    CSS SuitInput Component

--- src/components/Header/SuitInput.tsx
+++ src/components/Header/SuitInput.tsx
@@ -1,30 +1,32 @@
 import { type numberOfDecks } from "../../types";
 
-
 type Props = {
-    deckNumberController: [numberOfDecks, React.Dispatch<React.SetStateAction<numberOfDecks>>]
-}
+  deckNumberController: [numberOfDecks, React.Dispatch<React.SetStateAction<numberOfDecks>>];
+};
 
 export function SuitInput({ deckNumberController }: Props) {
-
   function handleSuitInput(e: React.ChangeEvent<HTMLInputElement>) {
-    const inputElement = e.currentTarget
-    const inputValue = Number(inputElement.value) as numberOfDecks
-    deckNumberController[1](inputValue)
+    const inputElement = e.currentTarget;
+    const inputValue = Number(inputElement.value) as numberOfDecks;
+    deckNumberController[1](inputValue);
   }
 
   return (
     <section className="suits-container">
-      <p className="suit-title">Numero de Barajas</p>
+      <p className="suit-title">
+        Numero <br />
+        de <br />
+        Barajas
+      </p>
       <div className="suit-option">
-        <label htmlFor="">
-          3
-          <input type="radio" name="deckNumber" value={3} checked={deckNumberController[0] === 3? true: false } onChange={handleSuitInput} />
-        </label>
-        <label htmlFor="">
-          4
-          <input type="radio" name="deckNumber" value={4} checked={deckNumberController[0] === 4? true: false } onChange={handleSuitInput} />
-        </label>
+        <div>
+          <label htmlFor="deck3">3</label>
+          <input type="radio" name="deckNumber" value={3} checked={deckNumberController[0] === 3 ? true : false} onChange={handleSuitInput} id='deck3'/>
+        </div>
+        <div>
+          <label htmlFor="deck4">4</label>
+          <input type="radio" name="deckNumber" value={4} checked={deckNumberController[0] === 4 ? true : false} onChange={handleSuitInput} id='deck4'/>
+        </div>
       </div>
     </section>
   );
